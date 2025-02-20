933b39829cc6a7e6f7fe1ebce2005320fdae28d3
kimanzialu <k.imanzi@alustudent.com>
2025-02-19 17:22:24 -0800

    v

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,9 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
-            print(f"{i}{j}", end=", " if i < 8 else "\n")
+            # Use format() for string formatting
+            if i == 8 and j == 9:
+                print("{:02d}{:02d}".format(i, j))
+            else:
+                print("{:02d}{:02d}".format(i, j), end=", ")
 
