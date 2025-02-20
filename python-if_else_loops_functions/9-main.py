03872e03afdbd696c871d3dbc8d8afab1f2ba54e
kimanzialu <k.imanzi@alustudent.com>
2025-02-20 04:02:15 +0200

    Delete python-if_else_loops_functions/9-main.py

--- python-if_else_loops_functions/9-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-# Test cases
-print_last_digit(98)  # Expecting 8
-print_last_digit(0)   # Expecting 0
-r = print_last_digit(-1024)  # Expecting 4
-print(r)  # Should print 4 (value returned by the function)
-
