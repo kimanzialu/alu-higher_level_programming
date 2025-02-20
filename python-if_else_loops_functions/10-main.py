5eac0a3e019b4baa26760b042742314019a075b6
kimanzialu <k.imanzi@alustudent.com>
2025-02-19 18:05:23 -0800

    rr

---
+++ python-if_else_loops_functions/10-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+add = __import__('10-add').add
+
+# Test cases
+print(add(1, 2))    # Expected output: 3
+print(add(98, 0))   # Expected output: 98
+print(add(100, -2)) # Expected output: 98
+
