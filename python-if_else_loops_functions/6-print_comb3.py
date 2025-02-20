0c07d6c6ea393b3d08a795e2ac886e48e30256c9
kimanzialu <k.imanzi@alustudent.com>
2025-02-19 17:24:25 -0800

    v

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
-            # Use format() for string formatting
+            # Use format() for string formatting with two digits only
             if i == 8 and j == 9:
                 print("{:02d}{:02d}".format(i, j))
             else:
