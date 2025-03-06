ab9bba4f336c828748d5070b7c12c9c4375b4ad2
kimanzialu <k.imanzi@alustudent.com>
2025-03-06 14:29:55 -0800

    rtyu

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
 for i in range(10):
     for j in range(i + 1, 10):
-        # Format both numbers to be two digits
-        if i == 8 and j == 9:
-            print("{:02d}{:02d}".format(i, j))  # Last pair without comma
-        else:
-            print("{:02d}{:02d}".format(i, j), end=", ")
-
+        print("{}".format(i) + "{}".format(j),
+              end=", " if i < 8 or j < 9 else "\n")
