6e0be6e2e2ca585992457f3d8fd8094ce7937882
kimanzialu <k.imanzi@alustudent.com>
2025-04-02 08:52:26 -0700

    Commit

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials.
-
-Usage: ./10-my_github.py <GitHub username> <GitHub password>
-  - Uses Basic Authentication to access the ID.
+""" A script that:
+- accepts your GitHub credentials (username and password),
+- uses the GitHub API to display your user ID.
 """
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
-
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
