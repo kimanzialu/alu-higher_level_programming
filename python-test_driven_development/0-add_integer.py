1ea2e834560acd472e9ca942ea8456dbea12f61f
kimanzialu <k.imanzi@alustudent.com>
2025-04-02 09:23:23 -0700

    Commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Implements a function for adding integers."""
+
+
+def add_integer(a, b=98):
+    """Calculate the sum of a and b as integers.
+
+    Any float arguments are converted to integers prior to the addition.
+
+    Raises:
+        TypeError: If a or b is neither an integer nor a float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
